73a037e8ae5d815ead8272cb398d46d3421cb53d
fzheng <feng.zheng@freecause.com>
2013-10-25 18:43:19 -0400

    add simplegrid and push

--- application/views/auth.php
+++ application/views/auth.php
@@ -1,19 +1,96 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<link rel="stylesheet" href="www/css/main.css" type="text/css" />
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+	<title>Marketingbazar: our slogan, our service, our future</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7; IE=EmulateIE9"> 
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+	<link rel="stylesheet" href="./www/css/base.css" type="text/css" media="all">
+	<!--[if lt IE 9 ]>
+    <link rel="stylesheet" href="./www/css/720_grid.css" type="text/css"><![endif]-->
+	<link rel="stylesheet" href="./www/css/720_grid.css" type="text/css" media="screen and (min-width: 720px)">
+	<link rel="stylesheet" href="./www/css/986_grid.css" type="text/css" media="screen and (min-width: 986px)">
+	<link rel="stylesheet" href="./www/css/1236_grid.css" media="screen and (min-width: 1236px)" >
+    <script type="text/javascript" src="./www/js/picturefill.js"></script>
 </head>
 <body>
-	<div id="container">
-		<div id="body">
-		<h1>Welcome to Marketingbazar!</h1>
-			<h2><a href="/main/oauth2/facebook">Login with Facebook</a></h2>
-			<h2><a href="/main/oauth/linkedin">Login with Linkedin</a></h2>
-			<h2><a href="/main/oauth2/google">Login with Google</a></h2>
-		</div>
-		<p class="footer">
-			Page rendered in <strong>{elapsed_time}</strong> seconds
-		</p>
+<div class="banner-wrap">
+	<div class="banner">
+		<h1 id="logo"><span class="logo">Marketingbazar</span></h1>
+		<p class="intro">Simply the <strong>Best</strong> Marketing Automation Place ever Exists</p>
 	</div>
-</body>
+</div>
+<div class="grid">
+    <div class="row">
+        <div class="slot-0-1-2-3 feature">
+            <span data-picture data-alt="Welcome, My Friend">
+			    <span data-src="./www/images/mainlogox240.jpg"></span>
+			    <span data-src="./www/images/mainlogox500.jpg" data-media="(min-width: 400px)"></span>
+			    <span data-src="./www/images/mainlogox640.jpg" data-media="(min-width: 800px)"></span>
+			    <span data-src="./www/images/mainlogox1024.jpg" data-media="(min-width: 1000px)"></span>
+			    <!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
+			    <noscript><img src="./www/images/mainlogox240.jpg" alt="Welcome, My Friend"></noscript>
+		    </span>
+        </div>
+        <div class="slot-4-5 feature">
+            <div class="row">
+                <h3><a href="/main/oauth2/facebook">Login with Facebook</a></h3>
+            </div>
+            <div class="row">
+                <h3><a href="/main/oauth/linkedin">Login with Linkedin</a></h3>
+            </div>
+            <div class="row">
+                <h3><a href="/main/oauth2/google">Login with Google</a></h3>
+            </div>
+        </div>
+    </div>
+</div>
+<hr />
+<div class="grid">
+	<div class="row">
+		<div class="slot-6 feature">
+			<h3>Customers</h3>
+            <p><a href="/">Launch competitions</a></p>
+            <p><a href="/">How it works</a></p>
+            <p><a href="/">FAQs</a></p>
+		</div>
+		<div class="slot-7 feature">
+            <h3>Consultants</h3>
+            <p><a href="/">Join competitions</a></p>
+            <p><a href="/">How it works</a></p>
+            <p><a href="/">FAQs</a></p>
+		</div>
+		<div class="slot-8 feature">
+            <h3>About Us</h3>
+            <p><a href="/">Our team</a></p>
+            <p><a href="/">Customers</a></p>
+            <p><a href="/">Blog</a></p>
+            <p><a href="/">Contact us</a></p>
+		</div>
+        <div class="slot-9 feature">
+            <h3>Community</h3>
+            <p><a href="http://www.facebook.com">Facebook</a></p>
+            <p><a href="http://www.linkedin.com">Linkedin</a></p>
+            <p><a href="http://www.twitter.com">Twitter</a></p>
+        </div>
+	</div>
+</div>
+<div class="empty"></div>
+<div class="row footer">
+    <div class="grid">
+        <div class="row">
+            <div class="slot-0-1">
+                <p><a href="/">Terms & Conditions</a></p>
+            </div>
+            <div class="slot-2-3">
+                <p><a href="/">Privacy Policy</a></p>
+            </div>
+            <div class="slot-4-5">
+                <p>&copy; <a href="http://www.marketingbazar.com">Marketingbazar 2013</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- / .grid -->
+</body> 
 </html>
